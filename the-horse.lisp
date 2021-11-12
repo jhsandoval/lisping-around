c9d40a136fc191fc137fd19d682d016d60a14cc0
jhsandoval <john.henry.sandoval@gmail.com>
2021-11-12 14:36:28 -0700

    next commit

--- the-horse.lisp
+++ the-horse.lisp
@@ -1,2 +1,7 @@
 (defun greet ()
   (format t "i've heard of pasta point of no return but this is ~a" "ridiculous"))
+
+(defparameter nums (loop for i from 1 to 10 collect i))
+
+(defun list-nums (nms)
+  (loop for i in nms do (princ i)))
